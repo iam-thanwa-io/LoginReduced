3bc4267c18dce4e99bf772573ba207e809c29ee4
Thanwa Ananthaseth <iam@thanwa.io>
2017-11-12 01:19:01 +0000

    Add babel preset

--- src-test/example.js
+++ src-test/example.js
@@ -9,39 +9,42 @@ const oauth = new OAuth2Server({
 let request = new Request({
   method: 'GET',
   query: {},
-  headers: { Authorization: 'Bearer foobar' }
+  headers: {Authorization: 'Bearer foobar'}
 });
 
 let response = new Response({
   headers: {}
 });
 
-oauth.authenticate(request, response).then(token => {
-  // The request was successfully authenticated.
-}).catch(err => {
-  // The request failed authentication.
-});
-
+oauth.authenticate(request, response)
+  .then((token) => {
+    // The request was successfully authenticated.
+  })
+  .catch((err) => {
+    // The request failed authentication.
+  });
+  
 const model = {
   // We support returning promises.
-  getAccessToken: function () {
+  getAccessToken: function() {
     return new Promise('works!');
   },
 
   // Or, calling a Node-style callback.
-  getAuthorizationCode: function (done) {
+  getAuthorizationCode: function(done) {
     done(null, 'works!');
   },
 
   // Or, using generators.
-  getClient: function* () {
+  getClient: function*() {
     yield somethingAsync();
     return 'works!';
   },
 
   // Or, async/wait (using Babel).
-  getUser: async function () {
+  getUser: async function() {
     await somethingAsync();
     return 'works!';
   }
 };
+
