1b176fe16790815646132cd92f8812907c9814ee
Thanwa Ananthaseth <iam@thanwa.io>
2017-11-12 01:13:55 +0000

    Add babel

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # node-oauth2-ready
 oauth2 implementation in a simple way
+
+
--- test/example.js
+++ test/example.js
@@ -23,3 +23,28 @@ oauth.authenticate(request, response)
   .catch((err) => {
     // The request failed authentication.
   });
+  
+const model = {
+  // We support returning promises.
+  getAccessToken: function() {
+    return new Promise('works!');
+  },
+
+  // Or, calling a Node-style callback.
+  getAuthorizationCode: function(done) {
+    done(null, 'works!');
+  },
+
+  // Or, using generators.
+  getClient: function*() {
+    yield somethingAsync();
+    return 'works!';
+  },
+
+  // Or, async/wait (using Babel).
+  getUser: async function() {
+    await somethingAsync();
+    return 'works!';
+  }
+};
+
