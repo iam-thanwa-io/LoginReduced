6c632a777336dfcd2253db2bc1031dfca2570783
Thanwa Ananthaseth <iam@thanwa.io>
2017-11-16 02:50:26 +0000

    Add express-oauth-server for test

--- server.js
+++ server.js
@@ -13,23 +13,48 @@ var express = require('express'),
     oauthserver = require('oauth2-server');
  
 var app = express();
- 
+
+  // app.use(express.static('public'));
+  // app.use(express.cookieParser());
+  // app.use(express.bodyParser());
+  // app.use(express.session({ secret: 'keyboard cat' }));
+  // app.use(passport.initialize());
+  // app.use(passport.session());
+  // app.use(app.router);
+  
+  
+//   var session = require("express-session"),
+//     bodyParser = require("body-parser");
+
+// app.use(express.static("public"));
+// app.use(session({ secret: "cats" }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+app.set('port', (process.env.PORT || 8080));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
- 
 app.use(bodyParser.json());
  
-app.oauth = oauthserver({
+app.oauth = new oauthserver({
   model: {}, // See below for specification 
   grants: ['password'],
   debug: true
 });
  
-app.all('/oauth/token', app.oauth.grant());
+// app.all('/oauth/token', app.oauth.grant());
  
 app.get('/', app.oauth.authorise(), function (req, res) {
   res.send('Secret area');
 });
- 
+
+app.use(app.oauth.authorize());
 app.use(app.oauth.errorHandler());
- 
-app.listen(3000);
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
